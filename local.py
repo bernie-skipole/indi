b805d94e40c8523cc41e166bd94d6198faaa8677
bernie-skipole <bernie@skipole.co.uk>
2020-06-16 23:56:54 +0100

    Working on documentation and local.py

---
+++ local.py
@@ -0,0 +1,21 @@
+#!/home/indi/indienv/bin/python3
+
+
+# Alter the above shebang to point to your own Python location
+
+"""Example script to run mqttredis"""
+
+
+from indimqttredis import mqttredis, mqtt_server, redis_server
+
+# define the hosts/ports where servers are listenning, these functions return named tuples.
+
+mqtt_host = mqtt_server(host='10.34.167.1', port=1883, username='', password='', to_indi_topic='to_indi', from_indi_topic='from_indi')
+redis_host = redis_server(host='localhost', port=6379, db=0, password='', keyprefix='indi_')
+
+# blocking call which runs the service, communicating between mqtt and redis
+
+mqttredis.run(mqtt_host, redis_host)
+
+
+
