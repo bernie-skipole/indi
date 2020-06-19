42b8eb6244cc19c6e2b80fb7179689fbf00f118b
bernie-skipole <bernie@skipole.co.uk>
2020-06-19 21:39:01 +0100

    Working on parsetypes numbervector

--- indimqttredis/fromxml/parsetypes.py
+++ indimqttredis/fromxml/parsetypes.py
@@ -1,5 +1,8 @@
 
 
+from datetime import datetime
+
+
 # When a Client first starts up it knows nothing about the Devices and Properties it will control. It begins by connecting to a
 # Device or indiserver and sending the getProperties command. This includes the protocol version and may include the name
 # of a specific Device and Property if it is known by some other means. If no device is specified, then all devices are reported; if
@@ -67,14 +70,12 @@ class ParseProperty():
         self.state = kwds.pop("state")      # current state of Property; Idle, OK, Busy or Alert
 
         # implied properties
-        self.label = kwds.pop("label", self.name)   # GUI label, use name by default
-        self.group = kwds.pop("group", "")     # Property group membership, blank by default
-        self.timestamp = kwds.pop("timestamp", 0) # moment when these data were valid
-                                                  ###### # set the default to a real timestamp  ##############!!!!!
+        self.label = kwds.pop("label", self.name)                             # GUI label, use name by default
+        self.group = kwds.pop("group", "")                                    # Property group membership, blank by default
+        self.timestamp = kwds.pop("timestamp", datetime.utcnow().isoformat()) # moment when these data were valid
         self.message = kwds.pop("message", "")
 
-        if kwds:
-            kwds.clear()                      # remove any unknown arguments, remove if further parent classes are to be used
+        kwds.clear()                      # clear any unknown arguments, remove if further parent classes are to be used
 
         super().__init__(**kwds)
 
@@ -101,8 +102,7 @@ class ParseElement():
     def __init__(self, **kwds):
         self.name = kwds.pop("name")                # name of the element, required value
         self.label = kwds.pop("label", self.name)   # GUI label, use name by default
-        if kwds:
-            kwds.clear()                       # remove any unknown arguments, remove if further parent classes are to be used
+        kwds.clear()                                # clear any unknown arguments, remove if further parent classes are to be used
         super().__init__(**kwds)
 
 
@@ -116,10 +116,10 @@ class ParseTextVector(ParseProperty):
         perm = kwds.pop("perm")
         self._set_permission(perm)              # ostensible Client controlability
         self.timeout = kwds.pop("timeout", 0)   # worse-case time to affect, 0 default, N/A for ro
-        self.elements(value)                
+        self._set_elements(value)
         super().__init__(**kwds)
 
-    def elements(self, value):
+    def _set_elements(self, value):
         "value is the xml defTextVector, this sets its child elements"
         self.element_list = []
         for child in value:
@@ -131,12 +131,12 @@ class ParseTextVector(ParseProperty):
         if not self.element_list:
             return ""
         for element in self.element_list:
-            return str(element)+"\n"
+            return element.label + " : " + str(element)+"\n"
 
 
 
 class ParseText(ParseElement):
-    "text items contained in a ParseTextVector"
+    "text elements contained in a ParseTextVector"
 
     def __init__(self, value, **kwds):
         if value is None:
@@ -146,7 +146,7 @@ class ParseText(ParseElement):
         super().__init__(**kwds)
 
     def __str__(self):
-        return f"{self.label} : {self.value}"
+        return self.value
 
 
 
@@ -159,8 +159,156 @@ class ParseNumberVector(ParseProperty):
         perm = kwds.pop("perm")
         self._set_permission(perm)              # ostensible Client controlability
         self.timeout = kwds.pop("timeout", 0)   # worse-case time to affect, 0 default, N/A for ro
-        self.value = value
+        self._set_elements(value)
         super().__init__(**kwds)
+
+    def _set_elements(self, value):
+        "value is the xml defNumberVector, this sets its child elements"
+        self.element_list = []
+        for child in value:
+            number_element = ParseNumber(child.text, **child.attrib)
+            self.element_list.append(number_element)
+
+    def __str__(self):
+        "Creates a string of label:number lines"
+        if not self.element_list:
+            return ""
+        for element in self.element_list:
+            return element.label + " : " + str(element)+"\n"
+
+
+
+class ParseNumber(ParseElement):
+    "number elements contained in a defNumberVector"
+
+    def __init__(self, value, **kwds):
+        # required number attributes
+        self.format = kwds.pop("format")    # printf-style format for GUI display
+        self.min = kwds.pop("format")       # minimal value
+        self.max = kwds.pop("format")       # maximum value, ignore if min == max
+        self.step = kwds.pop("format")      # allowed increments, ignore if 0
+
+        # get the raw self.value
+        # and self.formatted_number
+        self._number(value)
+        super().__init__(**kwds)
+
+    def _number(self, value):
+        """Splits the number into a negative flag and three sexagesimal parts
+           then calls self._sexagesimal or self._printf to create the formatted string"""
+        value = value.strip()
+        self.value = value   # the raw value
+        # negative is True, if the value is negative
+        negative = value.startswith("-")
+        if negative:
+            value = value.lstrip("-")
+        # Is the number provided in sexagesimal form?
+        if " " in value:
+            parts = value.split(" ")
+        elif ":" in value:
+            parts = value.split(":")
+        elif ";" in value:
+            parts = value.split(";")
+        else:
+            # not sexagesimal
+            parts = [value, "0", "0"]
+        # Any missing parts should have zero
+        if len(parts) == 2:
+            # assume seconds are missing, set to zero
+            parts.append("0")
+        assert len(parts) == 3
+        number_strings = list(x if x else "0" for x in parts)
+        # convert strings to integers or floats
+        number_list = []
+        for part in number_strings:
+            try:
+                num = int(part)
+            except ValueError:
+                num = float(part)
+            number_list.append(num)
+        # convert the number to a formatted string
+        if self.format.startswith("%") and self.format.endswith("m"):
+            self._sexagesimal(negative, number_list)
+        else:
+            self._printf(negative, number_list)
+
+
+    def _sexagesimal(self, negative, number_list):
+        "Create string of the number according to the given format"
+       # degrees and minutes should be integers
+       if not isinstance(number_list[0], int):
+           # its a float, so get integer part and fraction part
+           fractdegrees, degrees = math.modf(number_list[0])
+           number_list[0] = int(degrees)
+           number_list[1] += 60*fractdegrees
+       if not isinstance(number_list[1], int):
+           # its a float, so get integer part and fraction part
+           fractminutes, minutes = math.modf(number_list[1])
+           number_list[1] = int(minutes)
+           number_list[2] += 60*fractminutes
+        # Ensure minutes and seconds are less than 60
+        minutes = 0        
+        while number_list[2] >= 60:
+            number_list[2] -= 60
+            minutes += 1
+        number_list[1] += minutes
+        degrees = 0
+        while number_list[1] >= 60:
+            number_list[1] -= 60
+            degrees += 1
+        number_list[0] += degrees
+
+        # so number list is a valid degrees, minutes, seconds
+
+        # degrees
+        if negative:
+            number = f"-{number_list[0]}:"
+        else:
+            number = f"{number_list[0]}:"
+
+        # format string is of the form  %<w>.<f>m
+        w,f = self.format.split(".")
+        w = w.lstrip("%")
+        f = f.rstrip("m")
+
+        if (f == "3") or (f == "5"):
+            # no seconds, so create minutes value
+            minutes = float(number_list[1]) + number_list[2]/60.0
+            if f == "5":
+                number += f"{minutes:04.1f}"
+            else:
+                number += f"{minutes:02.0f}"
+        else:
+            number += f"{number_list[1]:02d}:"
+            seconds = float(number_list[2])
+            if f == "6":
+                number += f"{seconds:02.0f}"
+            elif f == "8":
+                number += f"{seconds:04.1f}"
+            else:
+                number += f"{seconds:05.2f}"
+
+        # w is the overall length of the string, prepend with spaces to make the length up to w
+        w = int(w)
+        l = len(number)
+        if w>l:
+            number = " "*(w-l) + number
+
+        self.formatted_number = number
+
+
+    def _printf(self, negative, number_list):
+        "Create string of the number according to the given format"
+        value = number_list[0] + (number_list[1]/60) + (number_list[2]/360)
+        if negative:
+            value = -1 * value
+        value_string = "{:" + self.format + "}"
+        self.formatted_number = value_string.format(value)
+
+
+    def __str__(self):
+        "Returns the string of the number using the format value"
+        return self.formatted_number
 
 
 
@@ -214,11 +362,19 @@ class ParseBLOBVector(ParseProperty):
 
 def receive_tree(root, rconn):
     "Receives the element tree root"
+    devices = set()       # create a set of devices
     for child in root:
         if child.tag == "defTextVector":
             text_vector = ParseTextVector(child, **child.attrib)
+            devices.add(text_vector.device)
             print(text_vector.device, text_vector.name)
             print(str(text_vector))
+        if child.tag == "defNumberVector":
+            number_vector = ParseNumberVector(child, **child.attrib)
+            devices.add(number_vector.device)
+            print(number_vector.device, number_vector.name)
+            print(str(number_vector))
+
 
 
 
