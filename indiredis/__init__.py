d202c60eb990a8d73caa5dd9d3e25149520d18a9
bernie-skipole <bernie@skipole.co.uk>
2020-09-16 21:47:00 +0100

    added file i_to_m.py

--- indiredis/__init__.py
+++ indiredis/__init__.py
@@ -59,9 +59,7 @@ MQTTServer = collections.namedtuple('MQTTServer', ['host', 'port', 'username', '
 
 # make the function inditoredis available to scripts importing this module
 from .i_to_r import inditoredis
-
-
-
+from .i_to_m import inditomqtt
 
 def indi_server(host='localhost', port=7624):
     "Creates a named tuple to hold indi server parameters"
@@ -93,117 +91,6 @@ def mqtt_server(host='localhost', port=1883, username='', password='', to_indi_t
 
 
 
-
-
-
-
-### MQTT Handlers for inditomqtt
-
-def _inditomqtt_on_message(client, userdata, message):
-    "Callback when an MQTT message is received"
-    global _TO_INDI
-    # we have received a message for the indiserver, put it into the _TO_INDI buffer
-    _TO_INDI.append(message.payload)
- 
-
-def _inditomqtt_on_connect(client, userdata, flags, rc):
-    "The callback for when the client receives a CONNACK response from the MQTT server, renew subscriptions"
-    global _TO_INDI
-    _TO_INDI.clear()  # - start with fresh empty _TO_INDI buffer
-    if rc == 0:
-        userdata['comms'] = True
-        # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
-        client.subscribe( userdata["to_indi_topic"], 2 )
-        print("MQTT client connected")
-    else:
-        userdata['comms'] = False
-
-
-def _inditomqtt_on_disconnect(client, userdata, rc):
-    "The MQTT client has disconnected, set userdata['comms'] = False, and clear out any data hanging about in _TO_INDI"
-    global _TO_INDI
-    userdata['comms'] = False
-    _TO_INDI.clear()
-
-
-
-def inditomqtt(indiserver, mqttserver):
-    "Blocking call that provides the indiserver - mqtt connection"
-    global _TO_INDI
-
-    if not MQTT_AVAILABLE:
-        print("Error - Unable to import the Python paho.mqtt.client package")
-        sys.exit(1)
-
-    # wait for five seconds before starting, to give mqtt and other servers
-    # time to start up
-    sleep(5)
-
-    print("inditomqtt started")
-
-    # create an mqtt client and connection
-    userdata={ "comms"           : False,        # an indication mqtt connection is working
-               "to_indi_topic"   : mqttserver.to_indi_topic,
-               "from_indi_topic" : mqttserver.from_indi_topic }
-
-    mqtt_client = mqtt.Client(userdata=userdata)
-    # attach callback function to client
-    mqtt_client.on_connect = _inditomqtt_on_connect
-    mqtt_client.on_disconnect = _inditomqtt_on_disconnect
-    mqtt_client.on_message = _inditomqtt_on_message
-    # If a username/password is set on the mqtt server
-    if mqttserver.username and mqttserver.password:
-        mqtt_client.username_pw_set(username = mqttserver.username, password = mqttserver.password)
-    elif mqttserver.username:
-        mqtt_client.username_pw_set(username = mqttserver.username)
-
-    # connect to the MQTT server
-    mqtt_client.connect(host=mqttserver.host, port=mqttserver.port)
-    mqtt_client.loop_start()
-
-
-
-
-    # set up socket connections to the indiserver
-    mysel = selectors.DefaultSelector()
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((indiserver.host, indiserver.port))
-        sock.setblocking(False)
-
-        # Set up the selector to watch for when the socket is ready
-        # to send data as well as when there is data to read.
-        mysel.register( sock, selectors.EVENT_READ | selectors.EVENT_WRITE )
-
-        print('waiting for I/O')
-
-        # get blocks of data from the indiserver and fill up this list
-        _FROM_INDI = []
-
-        while True:
-
-            for key, mask in mysel.select(timeout=0.1):    # blocks for .1 second
-                connection = key.fileobj
-
-                if mask & selectors.EVENT_READ:
-                    data = connection.recv(1024)
-                    if data:
-                        # A readable client socket has data
-                        _FROM_INDI.append(data)
-                elif _FROM_INDI:
-                    # no data to read, so gather the data received so far into a string
-                    from_indi = b"".join(_FROM_INDI)
-                    # and empty the _FROM_INDI list
-                    _FROM_INDI.clear()
-                    # send the payload via mqtt with topic from_indi_topic
-                    result = mqtt_client.publish(topic=mqttserver.from_indi_topic, payload=from_indi, qos=2)
-                    result.wait_for_publish()
-
-                if mask & selectors.EVENT_WRITE:
-                    if _TO_INDI:
-                        # Send the next message to the indiserver
-                        to_indi = _TO_INDI.popleft()
-                        sock.sendall(to_indi)
 
 
 
