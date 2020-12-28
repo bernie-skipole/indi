00dbed0ce900379646766bc46ebaeaa2bb0b8bcd
bernie-skipole <bernie@skipole.co.uk>
2020-12-28 11:21:15 +0000

    Removed old tests, opdated slides

--- tests/irtest7.py
+++
@@ -1,9 +0,0 @@
-from indiredis import driverstomqtt, mqtt_server
-
-# define the hosts/ports where servers are listenning, this function returns a named tuple.
-
-mqtt_host = mqtt_server(host='localhost', port=1883)
-
-# blocking call which runs the service, communicating between drivers and mqtt
-
-driverstomqtt(["indi_simulator_telescope", "indi_simulator_ccd"], 'indi_drivers01', mqtt_host)
